d786545491a9908bde307472523e59e6e677cc85
YoshiakiMiyabe <miya550111@outlook.jp>
2025-10-15 17:44:57 +0900

    軽微な修正

--- js/main.js
+++ js/main.js
@@ -1,44 +1,43 @@
 // ---------- ハンバーガーメニュー jQuery ----------
 $(function () {
-  const hamburger = $('.js-hamburger-menu');
-  const hamburgerBtn = $('.js-hamburger-button');
+  const hamburger = $(".js-hamburger-menu");
+  const hamburgerBtn = $(".js-hamburger-button");
 
   // ハンバーガーボタンをクリックしたら
-  hamburgerBtn.on('click', function () {
-    $(this).toggleClass('active');
-    hamburger.toggleClass('active');
+  hamburgerBtn.on("click", function () {
+    $(this).toggleClass("active");
+    hamburger.toggleClass("active");
   });
 
   // 画面のサイズが変わったら
-  $(window).on('resize', function () {
+  $(window).on("resize", function () {
     // ハンバーガーメニューを閉じる
-    hamburger.removeClass('active');
+    hamburger.removeClass("active");
     // ボタンも閉じる
-    hamburgerBtn.removeClass('active');
+    hamburgerBtn.removeClass("active");
   });
 
   // メニュー内のページ内リンクがクリックされたとき
-  $('.js-hamburger-menu a[href]').on('click', function () {
+  $(".js-hamburger-menu a[href]").on("click", function () {
     // メニューを閉じる
-    hamburger.removeClass('active');
+    hamburger.removeClass("active");
     // ボタンも閉じる
-    hamburgerBtn.removeClass('active');
+    hamburgerBtn.removeClass("active");
   });
 
   // 画面をスクロールしたら
   $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
       // メニューを閉じる
-      hamburger.removeClass('active');
+      hamburger.removeClass("active");
       // ボタンも閉じる
-      hamburgerBtn.removeClass('active');
+      hamburgerBtn.removeClass("active");
     }
   });
-
 });
 
 // ---------- スライダー スリック ----------
-$('.slick').slick({
+$(".slick").slick({
   autoplay: true, // 自動再生
   autoplaySpeed: 10000, // 自動再生の速さ(mm秒)
   infinite: true, // 最後まできたら最初に戻る
@@ -52,9 +51,9 @@ $('.slick').slick({
 
 // ---------- フェードインアニメーション ----------
 $(function () {
-  const fadeTargets = $('.js-fade-in');
+  const fadeTargets = $(".js-fade-in");
 
-  $(window).on('scroll', function () {
+  $(window).on("scroll", function () {
     const scroll = $(window).scrollTop();
     const windowHeight = $(window).height();
 
@@ -63,7 +62,7 @@ $(function () {
 
       // 要素が画面下から少し見え始めたら実行
       if (scroll > targetTop - windowHeight + 100) {
-        $(this).addClass('is-visible');
+        $(this).addClass("is-visible");
       }
     });
   });
